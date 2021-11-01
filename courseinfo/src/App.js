3f579df78e2a438da5a221e4d5c4a947e6e2e513
onowakow <nowakowskiowen08@gmail.com>
2021-11-01 17:30:14 -0500

    Refactored Header, Content, and Total

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <>
       <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
       <p>
-        {props.part1} {props.exercise1}
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
       </p>
     </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>{props.total}</p>
   )
 }
 
@@ -22,14 +39,16 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course} part1 = {part1} exercise1 = {exercises1} />
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content 
+        part1 = {part1} 
+        exercises1 = {exercises1} 
+        part2 = {part2}
+        exercises2 = {exercises2}
+        part3 = {part3}
+        exercises3 = {exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
