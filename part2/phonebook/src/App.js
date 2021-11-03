9ba8343015814088b50d29ed1dc8bf8ba301f67b
onowakow <nowakowskiowen08@gmail.com>
2021-11-03 16:03:01 -0500

    Added basic form functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,48 @@
 import React, { useState } from 'react'
 
+// Components
+const Person = ({name}) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
 const App = () => {
+
+  // State declarations
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  
+  // Handler functions
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/* Key is set to index. Not a good practice, but I lack guidance on better alternatives. */}
+      {persons.map( (person, i) => (
+        <Person key={i} name={person.name}/>
+      ))}
     </div>
   )
 }
