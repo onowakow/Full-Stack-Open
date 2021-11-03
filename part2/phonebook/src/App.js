79b06e0193327a2a83bf60654ca9450e2308ec9a
onowakow <nowakowskiowen08@gmail.com>
2021-11-03 16:48:43 -0500

    about to implement filter component

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 
 // Components
-const Person = ({name}) => {
+const Person = ({person}) => {
   return (
-    <p>{name}</p>
+    <p>{person.name} {person.number}</p>
   )
 }
 
@@ -11,7 +11,7 @@ const People = ({persons}) => {
   
   return (
     persons.map(( person ) => (
-      <Person key={person.name} name={person.name}/>
+      <Person key={person.name} person={person}/>
     ))
   )
 }
@@ -20,9 +20,13 @@ const App = () => {
 
   // State declarations
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
   
   // Handler functions
 
@@ -31,33 +35,48 @@ const App = () => {
 
     // Check if name exists
     const names = persons.map(person => person.name)
-    console.log(names)
     if (names.indexOf(newName) !== -1) {
       // Is this the best place for this alert function? Should it be in another component?
-      return alert(`${newName} already taken. Please select a different name.`)
+      return alert(`${newName} is already added to the phonebook. Please select a different name.`)
     }
 
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({
+      name: newName,
+      number: newNumber,
+    }))
+
+    setNewNumber('')
     setNewName('')
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  console.log(persons)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <h3>Filter shown with</h3>
+      
+      <h3>Add new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <People persons={persons} />
     </div>
   )
